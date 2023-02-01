6883b8737b5cfb359ad5b3d0a80987f753d8b69f
finjat97 <finjat@stud.ntnu.no>
2023-02-01 08:31:38 +0100

    added possibility to compare F_uu and F_ud for different distances. Made plotting optional.

--- spinstructure_numbers.py
+++ spinstructure_numbers.py
@@ -4,17 +4,15 @@ import time as timer
 from joblib import dump, load
 from tqdm import tqdm 
 import matplotlib.pyplot as plt
+import numba as nb
 
 t = 1 #hopping
-cps_1 = 0.4 #singlet pairing potential
-cps_3 = 0.2 #triplet pairing potential
+cps_1 = 0 #singlet pairing potential
+cps_3 = 0 #triplet pairing potential
 gamma = 0.3 #SOC strength
-jott = 5 #RKKY interaction strength
+jott = 3 #RKKY interaction strength
 mu = 0.5 #chemical potential
-sites =  10 #lattice sites
-spin_position = [3,7] #position of impurity spins
-
-parameters = [sites, mu, cps_1, cps_3, gamma, jott] + spin_position
+sites =  20 #lattice sites
 
 #fermi distribution
 def fermi_dis(energy):
@@ -23,36 +21,36 @@ def fermi_dis(energy):
     return 1/ (np.exp(energy/constant)+1)
 
 #energy including spin orbit coupling
-def xi(k_x, k_y, heli):
-    return -2*t*(np.cos(k_x)+np.cos(k_y)) - mu + heli*abs(gamma*(k_y - k_x))
+# def xi(k_x, k_y, heli):
+#     return -2*t*(np.cos(k[0])+np.cos(k[1])) - mu + heli*abs(gamma*(k[1] - k[0]))
 
 #supercondcuting gap   
-def delta(k_x,k_y):
-    return [[0,cps_1],[-cps_1, 0]] + [[(-k_y+1j*(-k_x))*cps_3,(0)*cps_3],[(0)*cps_3, (k_y+1j*(-k_x))*cps_3]]
+# def delta(k[0],k[1]):
+#     return [[0,cps_1],[-cps_1, 0],[(-k[1]+1j*(-k[0]))*cps_3,(0)*cps_3],[(0)*cps_3, (k[1]+1j*(-k[0]))*cps_3]]
 
 #energy including spin orbit coupling and superconductivity
 def E(k, heli):
     if heli > 0: index = 0
     else: index = heli
-    return np.sqrt(xi(k[0],k[1], heli)**2 + abs(delta(k[0], k[1])[index][index])**2)
+    return np.sqrt((-2*t*(np.cos(k[0])+np.cos(k[1])) - mu + heli*abs(gamma*(k[1] - k[0])))**2 + abs([[0,cps_1],[-cps_1, 0],[(-k[1]+1j*(-k[0]))*cps_3,(0)*cps_3],[(0)*cps_3, (k[1]+1j*(-k[0]))*cps_3]][index][index])**2)
 
 #components of eigenvectors
 def norm(k,heli):
     if heli > 0: index = 0
     else: index = heli
-    return np.sqrt((E(k,heli) + xi(k[0],k[1],heli))**2 + abs(delta(k[0], k[1])[index][index])**2)
+    return np.sqrt((E(k,heli) + (-2*t*(np.cos(k[0])+np.cos(k[1])) - mu + heli*abs(gamma*(k[1] - k[0]))))**2 + abs([[0,cps_1],[-cps_1, 0],[(-k[1]+1j*(-k[0]))*cps_3,(0)*cps_3],[(0)*cps_3, (k[1]+1j*(-k[0]))*cps_3]][index][index])**2)
 
 def nu(k, heli):
     normalisation = norm(k, heli)
     if normalisation == 0: normalisation = 0.001
-    return (E(k, heli) + xi(k[0],k[1],heli))/normalisation
+    return (E(k, heli) + (-2*t*(np.cos(k[0])+np.cos(k[1])) - mu + heli*abs(gamma*(k[1] - k[0]))))/normalisation
 
 def eta(k, heli):
     if heli > 0: index = 0
     else: index = heli
     normalisation = norm(k, heli)
     if normalisation == 0: normalisation = 0.001
-    return (delta(k[0],k[1])[index][index])/normalisation
+    return ([[0,cps_1],[-cps_1, 0],[(-k[1]+1j*(-k[0]))*cps_3,(0)*cps_3],[(0)*cps_3, (k[1]+1j*(-k[0]))*cps_3]][index][index])/normalisation
 
 #coefficients from the unitary transformation matrix S used in the Schrieffer-Wolff transformation
 def A(k, k_2 , heli, heli_2):
@@ -95,7 +93,7 @@ def D_y_pospos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
     k_2 = [k_2_x, k_2_y]
 
     constants = ((jott/sites)**2)/2
-    position = np.exp(1j*((k[0]-k_2[0])*(pos-pos_2))) #position only along x-axis
+    position = np.exp(-1j*((k[0]-k_2[0])*(pos[0]-pos_2[0])+(k[1]-k_2[1])*(pos[1]-pos_2[1]))) #position in 2D
 
     abs_k = np.sqrt(k[0]**2+k[1]**2)
     abs_k_2 = np.sqrt(k_2[0]**2+k_2[1]**2)
@@ -111,13 +109,13 @@ def D_y_pospos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
         energy += F_minus(f1, f2, k, k_2, +1, -1) - F_minus(f1, f2, k, k_2, -1, +1)
 
     return -constants*position*energy*phase
-
+# @nb.jit(nopython=True)
 def J_pospos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
 
     k = [k_x,k_y]
     k_2 = [k_2_x, k_2_y]
-    constants = (jott/sites)**2/2
-    position = np.exp(1j*((k[0]-k_2[0])*(pos-pos_2))) #position only along x-axis
+    constants = ((jott/sites)**2)/2
+    position = np.exp(-1j*((k[0]-k_2[0])*(pos[0]-pos_2[0])+(k[1]-k_2[1])*(pos[1]-pos_2[1]))) #position in 2D
 
     energy_x, energy_y, energy_z = 0, 0, 0
     for element in [[A,a], [B,b], [C,d], [D,c]]:
@@ -133,8 +131,8 @@ def D_y_negpos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
 
     k = [k_x,k_y]
     k_2 = [k_2_x, k_2_y]
-    constants = (jott/sites)**2/2
-    position = np.exp(1j*((k[0]-k_2[0])*(pos-pos_2))) #position only along x-axis
+    constants = ((jott/sites)**2)/2
+    position = np.exp(-1j*((k[0]-k_2[0])*(pos[0]-pos_2[0])+(k[1]-k_2[1])*(pos[1]-pos_2[1]))) #position in 2D
 
     abs_k = np.sqrt(k[0]**2+k[1]**2)
     abs_k_2 = np.sqrt(k_2[0]**2+k_2[1]**2)
@@ -151,12 +149,13 @@ def D_y_negpos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
 
     return -constants*position*phase*energy
 
+# @nb.jit(nopython=True)
 def J_negpos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
 
     k = [k_x,k_y]
     k_2 = [k_2_x, k_2_y]
-    constants = (jott/sites)**2/4
-    position = np.exp(1j*((k[0]-k_2[0])*(pos-pos_2))) #position only along x-axis
+    constants = ((jott/sites)**2)/4
+    position = np.exp(-1j*((k[0]-k_2[0])*(pos[0]-pos_2[0])+(k[1]-k_2[1])*(pos[1]-pos_2[1]))) #position in 2D
 
     abs_k = np.sqrt(k[0]**2+k[1]**2)
     abs_k_2 = np.sqrt(k_2[0]**2+k_2[1]**2)
@@ -167,6 +166,7 @@ def J_negpos(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
     phase_2 = ((k_2[1]-1j*k_2[0])/abs_k_2) * ((k[1]+1j*k[0])/abs_k)
 
     energy_x, energy_y, energy_z = 0, 0, 0
+    
     for element in [[A,b], [B,a], [C,d], [D,c]]:
         f1 = element[0]
         f2 = element[1]
@@ -180,8 +180,8 @@ def gamma_xy(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
 
     k = [k_x,k_y]
     k_2 = [k_2_x, k_2_y]
-    constants = (jott/sites)**2/4
-    position = np.exp(1j*((k[0]-k_2[0])*(pos-pos_2))) #position only along x-axis
+    constants = ((jott/sites)**2)/4
+    position = np.exp(-1j*((k[0]-k_2[0])*(pos[0]-pos_2[0])+(k[1]-k_2[1])*(pos[1]-pos_2[1]))) #position in 2D
 
     abs_k = np.sqrt(k[0]**2+k[1]**2)
     abs_k_2 = np.sqrt(k_2[0]**2+k_2[1]**2)
@@ -199,31 +199,38 @@ def gamma_xy(k_x, k_y ,k_2_x, k_2_y, pos, pos_2):
     return constants*position*energy*phase
 
 
-def main():
+def main(compare=False, distance=0, plotting=False):
 
-    name = 'free energy analytical/eigen'
-    for element in parameters:
-        name += '_'+str(np.round(element,1))
-    name += '.txt'
+    spin_position = [[sites//2,0],[0,sites//4]] #position of impurity spins
+    parameters = [sites, mu, cps_1, cps_3, gamma, jott] + spin_position
 
     # print(load(name), 'load')
 
     all_F, D_y, J_vec, Gamma, k_1_x, k_1_y, k_2_x, k_2_y = [], [], [], [] ,[], [], [], []
+    
+    start_time = timer.time()
 
     for item_x in list(np.arange(-np.pi, np.pi ,2*np.pi/(sites))):
-            for item_y in list(np.arange(-np.pi, np.pi ,2*np.pi/(sites))):
-                k_1_x += [item_x]
-                k_1_y += [item_y]
+        for item_y in list(np.arange(-np.pi, np.pi ,2*np.pi/(sites))):
+            k_1_x += [item_x]
+            k_1_y += [item_y]
     for item_x in list(np.arange(-np.pi, np.pi ,2*np.pi/(sites))):
         for item_y in list(np.arange(-np.pi, np.pi ,2*np.pi/(sites))):
             k_2_x += [item_x]
             k_2_y += [item_y]
     #k = list(np.arange(-np.pi, np.pi ,2*np.pi/(sites)))
+    k_time = timer.time()
+    print('k loops: ', round(k_time-start_time,4))
 
-    spin_orientations = [[[0,1/2,0],[0,1/2,0]], [[1/2,0,0],[1/2,0,0]], [[0,-1/2,0],[0,1/2,0]], [[-1/2,0,0],[1/2,0,0]], [[0,1/2,0],[0,-1/2,0]], [[1/2,0,0],[-1/2,0,0]], 
-    [[1/2,0,0],[0,1/2,0]], [[0,1/2,0],[1/2,0,0]], [[-1/2,0,0],[0,1/2,0]], [[0,-1/2,0],[1/2,0,0]], [[1/2,0,0],[0,-1/2,0]], [[0,1/2,0],[-1/2,0,0]],
-    [[1/2,0,0],[0,0,1/2]], [[1/2,0,0],[0,0,-1/2]], [[-1/2,0,0],[0,0,1/2]], [[-1/2,0,0],[0,0,-1/2]], [[0,1/2,0],[0,0,1/2]], [[0,-1/2,0],[0,0,1/2]],
-    [[0,1/2,0],[0,0,-1/2]], [[0,-1/2,0],[0,0,-1/2]], [[0,0,1/2],[0,0,1/2]],  [[0,0,-1/2],[0,0,1/2]],  [[0,0,1/2],[0,0,-1/2]],  [[0,0,-1/2],[0,0,-1/2]]]
+    if compare:
+        spin_orientations = [[[0,1/2,0],[0,1/2,0]],[[0,1/2,0],[0,-1/2,0]]]
+        spin_position = [[1,0], [distance,0]]
+
+    else:
+        spin_orientations = [[[0,1/2,0],[0,1/2,0]], [[1/2,0,0],[1/2,0,0]], [[0,-1/2,0],[0,1/2,0]], [[-1/2,0,0],[1/2,0,0]], [[0,1/2,0],[0,-1/2,0]], [[1/2,0,0],[-1/2,0,0]], 
+        [[1/2,0,0],[0,1/2,0]], [[0,1/2,0],[1/2,0,0]], [[-1/2,0,0],[0,1/2,0]], [[0,-1/2,0],[1/2,0,0]], [[1/2,0,0],[0,-1/2,0]], [[0,1/2,0],[-1/2,0,0]],
+        [[1/2,0,0],[0,0,1/2]], [[1/2,0,0],[0,0,-1/2]], [[-1/2,0,0],[0,0,1/2]], [[-1/2,0,0],[0,0,-1/2]], [[0,1/2,0],[0,0,1/2]], [[0,-1/2,0],[0,0,1/2]],
+        [[0,1/2,0],[0,0,-1/2]], [[0,-1/2,0],[0,0,-1/2]], [[0,0,1/2],[0,0,1/2]],  [[0,0,-1/2],[0,0,1/2]],  [[0,0,1/2],[0,0,-1/2]],  [[0,0,-1/2],[0,0,-1/2]]]
 
     for version in tqdm(range(len(spin_orientations))):
         spin = spin_orientations[version]
@@ -234,32 +241,55 @@ def main():
                         # free_energy = D_y_pospos(k_1, k_2, spin_position[0], spin_position[1])*(spin[0][2]*spin[1][0]- spin[0][0]*spin[1][2])
         D_y_pospos_result = list(map(D_y_pospos, k_1_x,k_1_y,k_2_x,k_2_y, [spin_position[0]]*len(k_1_x), [spin_position[1]]*len(k_1_x)))
         D_y_pospos_result = sum(list(map(lambda x: x*(spin[0][2]*spin[1][0]- spin[0][0]*spin[1][2]), D_y_pospos_result))) 
-        free_energy = D_y_pospos_result    
+        free_energy = D_y_pospos_result   
+        # D_pp_time = timer.time()
+        # # print('D_pp calc: ', round(D_pp_time-k_time,4))
                         # free_energy += sum(np.multiply(J_pospos(k_1,k_2, spin_position[0], spin_position[1]), heisenberg))
         J_pospos_result = list(map(J_pospos, k_1_x,k_1_y,k_2_x,k_2_y, [spin_position[0]]*len(k_1_x), [spin_position[1]]*len(k_1_x)))
         J_pospos_result = sum(list(map(lambda x,y,z: x+y+z, list(map(lambda x: x*heisenberg[0], [item[0] for item in J_pospos_result])),list(map(lambda x: x*heisenberg[1], [item[1] for item in J_pospos_result])),list(map(lambda x: x*heisenberg[2], [item[2] for item in J_pospos_result])))))
-        free_energy += J_pospos_result                
+        free_energy += J_pospos_result    
+        # # # J_pp_time = timer.time()
+        # # print('J_pp calc: ', round(J_pp_time-D_pp_time,4))            
                         # free_energy += D_y_negpos(k_1, k_2, spin_position[0], spin_position[1])*(spin[0][2]*spin[1][0]- spin[0][0]*spin[1][2])
         D_y_negpos_result = list(map(D_y_negpos, k_1_x,k_1_y,k_2_x,k_2_y, [spin_position[0]]*len(k_1_x), [spin_position[1]]*len(k_1_x)))
         D_y_negpos_result = sum(list(map(lambda x: x*(spin[0][2]*spin[1][0]- spin[0][0]*spin[1][2]), D_y_negpos_result))) 
         free_energy += D_y_negpos_result
+        # # # D_np_time = timer.time()
+        # # print('D_np calc: ', round(D_np_time-J_pp_time,4))
                         # free_energy += sum(np.multiply(J_negpos(k_1,k_2, spin_position[0], spin_position[1]), heisenberg))  
         J_negpos_result = list(map(J_negpos, k_1_x,k_1_y,k_2_x,k_2_y, [spin_position[0]]*len(k_1_x), [spin_position[1]]*len(k_1_x)))
         J_negpos_result = sum(list(map(lambda x,y,z: x+y+z, list(map(lambda x: x*heisenberg[0], [item[0] for item in J_negpos_result])),list(map(lambda x: x*heisenberg[1], [item[1] for item in J_negpos_result])),list(map(lambda x: x*heisenberg[2], [item[2] for item in J_negpos_result])))))      
-        free_energy += J_negpos_result                
+        free_energy += J_negpos_result     
+        # # # J_np_time = timer.time()
+        # # print('J_np calc: ', round(J_np_time-D_np_time,4))           
                         # free_energy += gamma_xy(k_1, k_2, spin_position[0], spin_position[1])*(spin[0][1]*spin[1][0]+ spin[0][0]*spin[1][1])
         gamma_xy_result = list(map(gamma_xy, k_1_x,k_1_y,k_2_x,k_2_y, [spin_position[0]]*len(k_1_x), [spin_position[1]]*len(k_1_x)))
         gamma_xy_result = sum(list(map(lambda x: x*(spin[0][1]*spin[1][0]+ spin[0][0]*spin[1][1]), gamma_xy_result))) 
         free_energy += gamma_xy_result
+        # # # gamma_time = timer.time()
+        # # print('gamma calc: ', round(gamma_time-J_np_time,4))
+
+        # # # one_time = timer.time()
+        # # print('one version: ', round(one_time-k_time,4))
         
         D_y += [D_y_negpos_result+D_y_pospos_result]
         J_vec += [J_negpos_result+J_pospos_result]
         Gamma += [gamma_xy_result]
         all_F += [free_energy]
         if free_energy.imag > 10**(-30): print(free_energy, spin)
-        
-    dump(all_F, name, compress=2)
+    
+    version_time = timer.time()
+    print('spin loop: ', round(version_time-k_time, 4))
 
+    name = 'free energy analytical/eigen'
+    for element in parameters:
+        name += '_'+str(np.round(element,1))
+    name += '.txt'
+    dump(all_F, name, compress=2)
+    # print('gamma', Gamma)
+    # print('d_y', D_y)
+    # print('J', J_vec)
+    
     configurations_label = []
 
     for version in range(len(spin_orientations)):
@@ -283,42 +313,42 @@ def main():
     labels =[]
     for entry in configurations_label:
         labels += ['('+entry[0]+entry[1]+')']
-    
-    plt.plot(D_y, label=r'$D_y$')
-    plt.plot(J_vec, label=r'$J$')
-    plt.plot(Gamma, label=r'$\Gamma$')
-    # plt.xticks(range(len(spin_orientations)), labels, rotation=45)
-    plt.legend()
-    name = 'coefficient_relations'
-    for element in parameters:
-        name += '_'+str(np.round(element,2))
-    name += '.png'
-    plt.savefig(name)
-    plt.show()
-    plt.clf()
-    
-    plt.plot(all_F)
-    plt.xlabel('spin configuration')
-    plt.ylabel('free energy in 1/t')
-    plt.xticks(range(len(spin_orientations)), labels, rotation=45)
-    plt.title('analytical free energy for different spin-orientations of two impurity spins \n for '+str(parameters[0])+r' sites with $U=$'+str(parameters[2])+r' and $V=$'+str(parameters[3])+', J='+str(parameters[5])+r', $\gamma=$'+str(round(parameters[4],2)))
-    plt.tight_layout()
-    # plt.legend()
-    plt.grid()
+    if plotting: 
+        plt.plot(D_y, label=r'$D_y$')
+        plt.plot(J_vec, label=r'$J$')
+        plt.plot(Gamma, label=r'$\Gamma$')
+        # plt.xticks(range(len(spin_orientations)), labels, rotation=45)
+        plt.legend()
+        plt.grid()
+        name = 'coefficient_relations'
+        for element in parameters:
+            name += '_'+str(np.round(element,2))
+        name += '.png'
+        plt.savefig(name)
+        plt.show()
+        plt.clf()
+        
+        plt.plot(all_F)
+        plt.xlabel('spin configuration')
+        plt.ylabel('free energy in 1/t')
+        plt.xticks(range(len(spin_orientations)), labels, rotation=45)
+        plt.title('analytical free energy for different spin-orientations of two impurity spins \n for '+str(parameters[0])+r' sites with $U=$'+str(parameters[2])+r' and $V=$'+str(parameters[3])+', J='+str(parameters[5])+r', $\gamma=$'+str(round(parameters[4],2)))
+        plt.tight_layout()
+        # plt.legend()
+        plt.grid()
+        name = 'analytical_spinstructure'
+        for element in parameters:
+            name += '_'+str(np.round(element,2))
+        name += '.png'
+        plt.savefig(name)
+        plt.clf()
 
-    name = 'analytical_spinstructure'
-    for element in parameters:
-        name += '_'+str(np.round(element,2))
-    name += '.png'
-    plt.savefig(name)
-    plt.clf()
-
-    print(max(D_y))
+    # print('max D_y: ', max(D_y))
 
     return all_F, parameters
 
 if __name__ == '__main__':
     start = timer.time()
 
-    result = main()
+    result = main(compare=True)
     print('total duration ', round(timer.time()-start, 4))
