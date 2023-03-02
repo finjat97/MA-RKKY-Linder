5ec1e77ad2b8c1f95338899961820459a53fb4de
finjat97 <finjat@stud.ntnu.no>
2023-03-02 18:43:27 +0100

    added cos(k_y) to triplet gap.

--- RKKY_diagonalization_ky.py
+++ RKKY_diagonalization_ky.py
@@ -7,10 +7,7 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
 
     sites_x, sites_y, mu, cps, tri, gamma, rkky, imp1, imp2 = parameters
     hopping = 1
-
-    attract = [cps]*sites_x*sites_y
-    attract_tri_up = [tri]*sites_x*sites_y
-    attract_tri_down = [tri]*sites_x*sites_y
+    
     if len(positions) > 0:
         position = positions 
     else:
@@ -19,7 +16,7 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
     # calculate all allowed k-values 
     k_values = np.arange(-np.pi, np.pi ,2*np.pi/(sites_y))
 
-    def matrix_elements(site, SOC_local, SOC_nn, off_diag_ham):
+    def matrix_elements(site, SOC_local, SOC_nn, off_diag_ham, attract, attract_tri_up, attract_tri_down):
         data, row_index, column_index = [] ,[], []
         # SOC_local = [-2*gamma*np.sin(k_y), -2*gamma*np.sin(k_y)]
         # SOC_nn = [-gamma, gamma]
@@ -54,7 +51,7 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
 
             #to higher neighbor 
             # hopping and triplet pairing
-            data += off_diag_ham + [-attract_tri_up[site//4], np.conj(attract_tri_down[site//4]),np.conj(attract_tri_up[site//4]), -(attract_tri_down[site//4])]# attract_tri_ham + [-np.conj(element) for element in attract_tri_ham]
+            data += off_diag_ham + [attract_tri_up[site//4], np.conj(attract_tri_down[site//4]), -np.conj(attract_tri_up[site//4]), -(attract_tri_down[site//4])]# attract_tri_ham + [-np.conj(element) for element in attract_tri_ham]
             row_index += [site, site+1, site+2, site+3] * 2
             column_index += [site+4, site+4+1, site+4+2, site+4+3] + [site+4+2, site+4+3, site+4, site+4+1]
             #SOC
@@ -68,7 +65,7 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
         if (site - 4) >=0:
             #to lower neighbor
             # hopping and triplet pairing
-            data += off_diag_ham + [(attract_tri_up[site//4]), -np.conj(attract_tri_down[site//4]),-np.conj(attract_tri_up[site//4]), attract_tri_down[site//4]]# [np.conj(element) for element in attract_tri_ham] +[-element for element in attract_tri_ham]
+            data += off_diag_ham + [-(attract_tri_up[site//4]), -np.conj(attract_tri_down[site//4]),np.conj(attract_tri_up[site//4]), attract_tri_down[site//4]]# [np.conj(element) for element in attract_tri_ham] +[-element for element in attract_tri_ham]
             row_index += [site, site+1, site+2, site+3] * 2
             column_index += [site-4, site-4+1, site-4+2, site-4+3]  + [site-4+2, site-4+3, site-4, site-4+1]
             #SOC
@@ -82,9 +79,12 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
         SOC_local = [-2*gamma*np.sin(k_y), -2*gamma*np.sin(k_y)]
         SOC_nn = [-gamma, gamma]
         off_diag_ham = [2*element *np.cos(k_y) for element in [-hopping, -hopping , hopping, hopping]]
+        attract = [cps*np.cos(k_y)]*sites_x*sites_y
+        attract_tri_up = [tri*np.cos(k_y)]*sites_x*sites_y
+        attract_tri_down = [tri*np.cos(k_y)]*sites_x*sites_y
 
-        # find entries for all sides of the system
-        site_loop = (list(map(matrix_elements, list(range(0, sites_x*4, 4)), [SOC_local]*len(range(0, sites_x*4, 4)), [SOC_nn]*len(range(0, sites_x*4, 4)), [off_diag_ham]*len(range(0, sites_x*4, 4)) )))
+        # find entries for all sites of the system
+        site_loop = (list(map(matrix_elements, list(range(0, sites_x*4, 4)), [SOC_local]*len(range(0, sites_x*4, 4)), [SOC_nn]*len(range(0, sites_x*4, 4)), [off_diag_ham]*len(range(0, sites_x*4, 4)), attract, attract_tri_up, attract_tri_down )))
         
         def d(x):
             return np.array(site_loop[x][0])
@@ -101,21 +101,39 @@ def diagonalize_hamiltonian(parameters, spin, positions = [], output=False):
         matrix = sparse.csr_matrix((data, (row_index, column_index))).todense()
 
         #return eigenvectors and eigenvalues for the given k
-        return np.linalg.eigh(matrix)
+        res = np.linalg.eigh(matrix)
+        eigvec = np.array(res[1])
+        eigval = res[0].reshape(4*sites_x,1)
+        res2 = np.concatenate((eigval, eigvec), axis=1)
+        #return np.linalg.eigh(matrix)
+        return res2
 
     #calculate eigenvectors and eigenvalues for all allowed k values
-    eigen = list(map(k_matrices, k_values))
+    eigen = (list(map(k_matrices, k_values)))
+    eigen2 = np.array(eigen) #shape = (N_y, 4*N_x, 4*n_x +1), first column is eigenvalues, N_y corresponds to k-values
 
-    return eigen
+    return eigen2
 
 def operator_coefficients(diagonal, k):
     ## define u and v to get hamiltonian on form of Fermi-gas (see notes "realspace.pdf")
     u_up, u_down, v_up, v_down = [], [], [], [] #lists of lists
 
-    u_up += [np.conj(element) for element in diagonal[k][1][range(0, len(diagonal[0][0]),4)]]#appending N_x coefficients for one site, one k and all energy eigenvalues
-    u_down += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),1)]]
-    v_up += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),2)]]
-    v_down += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),3)]]
+    # u_up += [np.conj(element) for element in diagonal[k][1][range(0, len(diagonal[0][0]),4)]]#appending N_x coefficients for one site, one k and all energy eigenvalues
+    # u_down += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),1)]]
+    # v_up += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),2)]]
+    # v_down += [np.conj(element) for element in diagonal[k][1][np.add(range(0, len(diagonal[0][0]),4),3)]]
 
+    u_up_indices = np.arange(0, diagonal.shape[2]-1, 4)
+    u_down_indices = np.arange(1, diagonal.shape[2]-1, 4)
+    v_up_indices = np.arange(2, diagonal.shape[2]-1, 4)
+    v_down_indices = np.arange(3, diagonal.shape[2]-1, 4)
+
+    u_up = diagonal[:, diagonal.shape[1]//2: , u_up_indices]
+    u_down = diagonal[:, diagonal.shape[1]//2: , u_down_indices]
+    v_up = diagonal[:, diagonal.shape[1]//2: , v_up_indices]
+    v_down = diagonal[:, diagonal.shape[1]//2: , v_down_indices]
 
     return u_up, u_down, v_up, v_down
+
+# diag = diagonalize_hamiltonian([20, 15, 0.5, 0.5, 0.1, 0.15, 2, 3, 7],  [[0,1/2,0],[1/2,0,0]])
+# operator_coefficients(diag, 5)
