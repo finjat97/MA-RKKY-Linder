87220a4da5bac03ea1c7f1d7aa8bcaa05540323c
finjat97 <finjat@stud.ntnu.no>
2023-03-02 19:18:09 +0100

    rewrote ldos and free energy with np.arrays.

--- observables.py
+++ observables.py
@@ -17,45 +17,67 @@ def density_of_states(eigen, coefficients, position, kvalues, output=False):
 
     site = position[0]
     sites_x  = position[1]
+    den = []
     
-    density = [] #list of densities for each energy
-    energies = np.arange(eigen[0][0][0],eigen[0][0][-1]+0.01,0.01) #list of discretize energy spectrum
+    energies = np.arange(np.min(eigen[:,:,0]).real,np.max(eigen[:,:,0]).real+0.01,0.01) #list of discretize energy spectrum
     offset = len(eigen[0][0])//2 # makes it easier to just choose coefficients belonging to positive eigenvalues
     eigenvalues = range(offset) # gives the indices for all eigenvalues (either positive or negative, depending on if offset is used or not)
-    
-    def uu(x):
-        return np.square(np.absolute(coefficients[x][0][site][0,np.add(eigenvalues, offset)]))
-    def ud(x):
-        return np.square(np.absolute(coefficients[x][1][site][0,np.add(eigenvalues, offset)]))
-    def vu(x):
-        return np.square(np.absolute(coefficients[x][2][site][0,np.add(eigenvalues, offset)]))
-    def vd(x):
-        return np.square(np.absolute(coefficients[x][3][site][0,np.add(eigenvalues, offset)]))
-    def dm(y):
-        return [delta_function(x) for x in np.add(energy, - eigen[y][0][np.add(eigenvalues, offset)])] 
-    def dp(y):
-        return [delta_function(x) for x in np.add(energy, eigen[y][0][np.add(eigenvalues, offset)])] 
+    # def uu(x):
+    #     return np.square(np.absolute(coefficients[x][0][site][0,np.add(eigenvalues, offset)]))
+    # def ud(x):
+    #     return np.square(np.absolute(coefficients[x][1][site][0,np.add(eigenvalues, offset)]))
+    # def vu(x):
+    #     return np.square(np.absolute(coefficients[x][2][site][0,np.add(eigenvalues, offset)]))
+    # def vd(x):
+    #     return np.square(np.absolute(coefficients[x][3][site][0,np.add(eigenvalues, offset)]))
+    # def dm(y):
+    #     return [delta_function(x) for x in np.add(energy, - eigen[y][0][np.add(eigenvalues, offset)])] 
+    # def dp(y):
+    #     return [delta_function(x) for x in np.add(energy, eigen[y][0][np.add(eigenvalues, offset)])] 
+
+
+    u_up, u_down, v_up, v_down = coefficients
+    u_up = np.absolute(np.square(u_up[:,:,site]) )
+    u_down = np.absolute(np.square(u_down[:,:,site] ))
+    v_up = np.absolute(np.square(v_up[:,:,site] ))
+    v_down = np.absolute(np.square(v_down[:,:,site] ))
+     
+    eigenenergies = eigen[:,:sites_x*2,0] #find it, make it format shape = (k,n) or (k,n,1)
+    us = u_up + u_down
+    vs = v_up + v_down
+
+   
+    #calculate deltas for different energies
+
+    def d_E(E):
+        density = sum(sum(us* delta_function(E+eigenenergies) + vs * delta_function(E-eigenenergies)))
+        return density/sites_x
 
     for energy in energies:
-        sum_1 , sum_2 = 0,0
-        # calculate coefficients for all positive k values
-        u_up_eigval = list(map(uu, kvalues[0]))
-        u_down_eigval = list(map(ud, kvalues[0]))
-        v_up_eigval = list(map(vu, kvalues[0]))
-        v_down_eigval = list(map(vd, kvalues[0]))
-        # calculate delta functions, which are prefactors, for all positive k values
-        deltas_plus = list(map(dp, kvalues[0]))
-        deltas_minus = list(map(dm, kvalues[0]))
-        # multiply the two different terms and add each one together, see numerical approach in master thesis for furthe explanation
-        sum_1 += np.sum(np.multiply(deltas_plus, u_down_eigval + u_up_eigval))
-        sum_2 += np.sum(np.multiply(deltas_minus, v_down_eigval + v_up_eigval))
+        den.append(d_E(energy))
 
-        density.append((sum_1 + sum_2)/sites_x)
-    
-    if output: 
-        print('__density of states__\n', [np.round(element,4) for element in density])
-        
-    return density
+    # for energy in energies:
+    #     sum_1 , sum_2 = 0,0
+    #     # calculate coefficients for all positive k values
+    #     u_up_eigval = list(map(uu, kvalues[0]))
+    #     u_down_eigval = list(map(ud, kvalues[0]))
+    #     v_up_eigval = list(map(vu, kvalues[0]))
+    #     v_down_eigval = list(map(vd, kvalues[0]))
+    #     # calculate delta functions, which are prefactors, for all positive k values
+    #     deltas_plus = list(map(dp, kvalues[0]))
+    #     deltas_minus = list(map(dm, kvalues[0]))
+    #     # multiply the two different terms and add each one together, see numerical approach in master thesis for further explanation
+    #     sum_1 += np.sum(np.multiply(deltas_plus, u_down_eigval + u_up_eigval))
+    #     sum_2 += np.sum(np.multiply(deltas_minus, v_down_eigval + v_up_eigval))
+
+    #     density.append((sum_1 + sum_2)/sites_x)
+    # print(energies.shape)
+    # res = d_E(energies)
+    # print(res)
+    # # if output: 
+    #     print('__density of states__\n', [np.round(element,4) for element in density])
+
+    return den, energies
 
 def selfconsistency_gap(eigen, potential, coefficients, kvalues):
     #potential is interaction strength of electrons, format: [singlet potential, triplet potential]
@@ -146,18 +168,31 @@ def selfconsistency_gap(eigen, potential, coefficients, kvalues):
     return [gap_singlet, [gap_triplet_up, gap_triplet_down, gap_triplet_ud, gap_triplet_du], gap_triplet] #, gap_triplet_left
 
 def free_energy(eigen, kvalues, output=False):
-    
     beta = 100 #1/k_b T
-    # find all combinations of k values and eigenvalue indices
-    args_p = np.array(np.meshgrid(kvalues[0], list(range(len(eigen[0][0]))))).T.reshape(-1,2)
-    args_0 = np.array(np.meshgrid(kvalues[1], list(range(len(eigen[0][0]))))).T.reshape(-1,2)
-    # make sure to only consider positive energies
-    args_0 = [item for item in args_0 if eigen[args_0[0][0]][0][args_0[0][1]]>0]
-    # how to calculate the energy
-    def energy(x):
-        return (- eigen[x[0]][0][x[1]])/ 2 - (np.log(1+np.exp(-beta*eigen[x[0]][0][x[1]])))/beta
-    # calculate free energy
-    free_energy = sum(map(energy, args_p)) + sum(map(energy, args_0))
+    eigvals = eigen[:,:,0]
+    # print(eigvals.shape)
+    eigvals = eigvals[eigvals.shape[0]//2:,:]
+    # print(eigvals.shape)
+    #print(kvalues[0])
+    # eigvals_posk = eigvals[kvalues[0]]
+    # sum_posk = sum( sum ( -1/2 * eigvals_posk - 1/beta * np.log( 1+ np.exp(-beta*eigvals_posk)) ))
+    # eigvals_zerok = eigvals[kvalues[1]]
+
+    # sum_zerok = sum (sum( -1/2 * eigvals_zerok - 1/beta * np.log( 1+ np.exp(-beta*eigvals_zerok)) ) )
+
+    # print(sum_zerok)
+    F = sum( sum ( -1/2 * eigvals - 1/beta * np.log( 1+ np.exp(-beta*eigvals)) ))
+    
+    # # find all combinations of k values and eigenvalue indices
+    # args_p = np.array(np.meshgrid(kvalues[0], list(range(len(eigen[0][0]))))).T.reshape(-1,2)
+    # args_0 = np.array(np.meshgrid(kvalues[1], list(range(len(eigen[0][0]))))).T.reshape(-1,2)
+    # # make sure to only consider positive energies
+    # args_0 = [item for item in args_0 if eigen[args_0[0][0]][0][args_0[0][1]]>0]
+    # # how to calculate the energy
+    # def energy(x):
+    #     return (- eigen[x[0]][0][x[1]])/ 2 - (np.log(1+np.exp(-beta*eigen[x[0]][0][x[1]])))/beta
+    # # calculate free energy
+    # free_energy = sum(map(energy, args_p)) + sum(map(energy, args_0))
 
     # for k in kvalues[0]: #all positive k-values
     #     # going through all eigenvalues
@@ -172,7 +207,7 @@ def free_energy(eigen, kvalues, output=False):
     if output:
         print('__free energy__\n', round(free_energy,3))
 
-    return free_energy
+    return F
 
 def interband_pairing(eigen, kvalues, coeffis):
 
